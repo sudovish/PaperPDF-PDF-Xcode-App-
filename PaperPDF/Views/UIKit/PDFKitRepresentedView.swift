908a9cb271f9e7300aa3d8478357e4dd56a7b5b9
sudovish <156399208+sudovish@users.noreply.github.com>
2026-08-07 17:22:01 -0700

    Add PDFKit SwiftUI bridge

---
+++ PaperPDF/Views/UIKit/PDFKitRepresentedView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import PDFKit
+
+struct PDFKitRepresentedView: UIViewRepresentable {
+    let document: PDFDocument
+
+    func makeUIView(context: Context) -> PDFView {
+        let view = PDFView()
+        view.autoScales = true
+        view.displayDirection = .vertical
+        view.displayMode = .singlePageContinuous
+        view.document = document
+        return view
+    }
+
+    func updateUIView(_ uiView: PDFView, context: Context) {
+        if uiView.document !== document {
+            uiView.document = document
+        }
+    }
+}
